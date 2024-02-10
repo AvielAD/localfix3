c53161831b53df94b720af3261e45a4da2bea0b8
Aviel Aldama <aviel.aldama@gmail.com>
2024-02-09 23:44:30 -0600

    home

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,8 +2,9 @@
 import Promocional from '@/Components/PromocionalLocalFix/page'
 import { useRef } from 'react';
 import { useReactToPrint } from 'react-to-print';
+import MenuAdd from "@/Components/AddMenu"
 
-const Dashboard = ()=>{
+const Dashboard = () => {
     const componentRef = useRef<HTMLDivElement>(null);
     const handlePrint = useReactToPrint({
         content: () => componentRef.current,
@@ -13,9 +14,34 @@ const Dashboard = ()=>{
         <div>
             prueba dashboard
 
-            <p>Imprimir Promocional <i onClick={handlePrint} className="bi bi-printer"></i></p>
+            <div className='row'>
+                <div className='col'>
+                    <p className='d-flex justify-content-center'><i onClick={handlePrint} className="display-4 bi bi-printer"></i></p>
+                </div>
+            </div>
+
             <div className='d-none'>
                 <Promocional ref={componentRef}></Promocional>
+            </div>
+
+            <div className='row'>
+                <div className='col'>
+                    <div className='d-flex justify-content-center'>
+                        <div>
+                            <MenuAdd url='/dashboard/diagnosticos/Add'></MenuAdd>
+                            <p>Diagnosticos</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col'>
+                    <div className='d-flex justify-content-center'>
+                        <div>
+                            <MenuAdd url='/dashboard/reparaciones/first'></MenuAdd>
+                            <p>Reparaciones</p>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
