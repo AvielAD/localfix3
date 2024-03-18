b22a40d4f61247c6f0cf2f02f83c3ad4032d2b95
Aviel Aldama <aviel.aldama@gmail.com>
2024-03-18 12:06:38 -0600

    bg color grey modal

--- src/Components/ModalGeneral/page.tsx
+++ src/Components/ModalGeneral/page.tsx
@@ -5,8 +5,8 @@ const Page = (props: modaldto) => {
         <>
 
             <div  className={`${props.show ? 'd-block' : 'd-none'}`}>
-                <div onClick={()=>props.close()} style={{ height: '100vh' }}
-                    className="w-100 z-1 position-absolute" >
+                <div onClick={()=>props.close()} style={{ height: '100vh', backgroundColor: 'rgba(0,0,255,0.5)'}}
+                    className="w-100 z-1 position-absolute bg-secondary" >
                     <div className="d-flex h-100 justify-content-center align-items-center">
                         <div onClick={(e) => e.stopPropagation()} className="w-75 h-100 bg-primary rounded-3">
                             <div className="d-flex justify-content-center">
