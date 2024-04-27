5989f71f7cbd3da77a51949852d6605b07fe86a4
Aviel Aldama <aviel.aldama@gmail.com>
2024-04-27 08:49:37 -0600

    style details

--- src/app/dashboard/reparaciones/details/[slug]/page.tsx
+++ src/app/dashboard/reparaciones/details/[slug]/page.tsx
@@ -22,64 +22,72 @@ const Details = ({ params }: { params: { slug: string } }) => {
     if (reparacionDetail.data) allInfo = reparacionDetail.data
 
     return (<>
-        <h2>Detalles 
+        <h2 className="text-center">Detalles</h2>
 
-        <button onClick={handlePrint}>
-            <i className="bi bi-printer"></i>
-        </button>
 
-        </h2>
         <div className="d-none">
 
-        <ComponentNota ref={componentRef}
-        nombreCliente={allInfo?.nombre+" "+allInfo?.apellido}
-        telCliente={allInfo?.telefono}
-        modeloEquipo={allInfo?.marca+" "+allInfo?.modelo}
-        fechaRecepcion={allInfo?.recepcion.toString().split("T")[0]}
-        fechaEntrega={allInfo?.entrega.toString().split("T")[0]}
-        descripcionFalla={allInfo?.falla}
-        descripcionReparacion={allInfo?.diagnostico}
-        costoTotal={ parseInt( allInfo?.total)}
-        ></ComponentNota>
+            <ComponentNota ref={componentRef}
+                nombreCliente={allInfo?.nombre + " " + allInfo?.apellido}
+                telCliente={allInfo?.telefono}
+                modeloEquipo={allInfo?.marca + " " + allInfo?.modelo}
+                fechaRecepcion={allInfo?.recepcion.toString().split("T")[0]}
+                fechaEntrega={allInfo?.entrega.toString().split("T")[0]}
+                descripcionFalla={allInfo?.falla}
+                descripcionReparacion={allInfo?.diagnostico}
+                costoTotal={parseInt(allInfo?.total)}
+            ></ComponentNota>
         </div>
 
+        <div className="container">
 
-        <div className="row">
-            <div className="col-6">
-                <h2>Cliente</h2>
-                <p>Nombre: {allInfo?.nombre}</p>
-                <p>Apellido: {allInfo?.apellido}</p>
-                <p>Telefono: {allInfo?.telefono}</p>
-            </div>
+            <div className="row">
+                <div className="col-4">
+                    <h2>Cliente</h2>
+                    <p>Nombre: {allInfo?.nombre}</p>
+                    <p>Apellido: {allInfo?.apellido}</p>
+                    <p>Telefono: {allInfo?.telefono}</p>
+                </div>
 
-            <div className="col-6">
-                <h2>Equipo</h2>
-                <p>Marca: {allInfo?.marca}</p>
-                <p>Modelo: {allInfo?.modelo}</p>
+                <div className="col-4">
+                    <h2>Equipo</h2>
+                    <p>Marca: {allInfo?.marca}</p>
+                    <p>Modelo: {allInfo?.modelo}</p>
 
-            </div>
+                </div>
+                <div className="col-4 text-justify">
+                    <h2>Costo</h2>
+                    <p>Presupuesto Inicial: ${allInfo?.presupuesto} mxn</p>
+                    <p>Costo Total: ${allInfo?.total} mxn</p>
 
-        </div>
-        <div className="row">
-            <div className="col-8">
-                <h2>Reparacion</h2>
-                <p>Recepcion: {allInfo?.recepcion.toString().split("T")[0]}</p>
-                <p>Entrega: {allInfo?.entrega.toString().split("T")[0]}</p>
-            </div>
+                </div>
 
-            <div className="col-4 text-justify">
-                <h2>Costo</h2>
-                <p>Presupuesto Inicial: ${allInfo?.presupuesto} mxn</p>
-                <p>Costo Total: ${allInfo?.total} mxn</p>
 
             </div>
+            <div className="row">
+                <div className="col-4">
+                    <h2>Reparacion</h2>
+                    <p>Recepcion: {allInfo?.recepcion.toString().split("T")[0]}</p>
+                    <p>Entrega: {allInfo?.entrega.toString().split("T")[0]}</p>
+                </div>
 
-        </div>
-        <div className="row">
-            <h2>Seguimiento</h2>
-            <div className="col">
-                <p> Falla Reportada: {allInfo?.falla}</p>
-                <p> Acciones a Resolver: {allInfo?.diagnostico}</p>
+                <div className="col-8">
+                    <h2>Seguimiento</h2>
+                    <p> Falla Reportada: {allInfo?.falla}</p>
+                    <p> Acciones a Resolver: {allInfo?.diagnostico}</p>
+                </div>
+
+            </div>
+
+            <div className="row">
+                <div className="col-4">
+
+                    <h2>Acciones</h2>
+                    <button className="btn btn-primary" onClick={handlePrint}>
+                        <i style={{ fontSize: '2rem' }} className="bi bi-printer">Ticket</i>
+                    </button>
+                </div>
+
             </div>
         </div>
 
