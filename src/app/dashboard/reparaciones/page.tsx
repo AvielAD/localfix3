f16a8c849a1fc10678bbf54c6532a4c817de53ec
Aviel Aldama <aviel.aldama@gmail.com>
2024-06-17 01:11:04 -0600

    material ui table

--- src/app/dashboard/reparaciones/page.tsx
+++ src/app/dashboard/reparaciones/page.tsx
@@ -4,9 +4,20 @@ import { useRouter } from "next/navigation"
 import { ReparacionDto } from "@/DTOS/reparaciones/reparacion"
 import MenuAdd from "@/Components/AddMenu"
 import { FormatMedDate } from "@/Utilities/DateTimeHelpers/FormattingDate"
-import { Button, Fab, Icon } from "@mui/material"
+
+import { Button, Fab, Grid, Icon } from "@mui/material"
 import { AddCircle } from "@mui/icons-material"
 import { Add } from "@mui/icons-material"
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Link from "next/link"
+
+
 const fetcher = (url: string) => fetch(url).then(r => r.json())
 const compareFecha = (a: ReparacionDto, b: ReparacionDto) => {
     if (a.recepcion > b.recepcion)
@@ -26,47 +37,54 @@ const Reparaciones = () => {
         repairData = diagnosticosData.data.sort(compareFecha)
     }
     return (<>
-        <h1 className="text-center">Reparaciones</h1>
-        <div className="d-flex justify-content-center">
-            <MenuAdd url='/dashboard/reparaciones/first'></MenuAdd>
-        </div>
+        <Grid container rowSpacing={2} >
+            <Grid item xs={12}>
+                <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+                    <TableContainer sx={{ maxHeight: '48rem' }} >
+                        <Table >
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Equipo</TableCell>
+                                    <TableCell>Recepcion</TableCell>
+                                    <TableCell>Entrega</TableCell>
+                                    <TableCell>Acciones</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {
+                                    repairData?.map((item: ReparacionDto, index: number) => {
+                                        return (
+                                            <TableRow key={index}>
+                                                <TableCell>{item.modelo}</TableCell>
+                                                <TableCell>{FormatMedDate(item.recepcion)}</TableCell>
+                                                <TableCell>{FormatMedDate(item.entrega)}</TableCell>
+                                                <TableCell>
+                                                    <i onClick={
+                                                        () => router.push(`/dashboard/reparaciones/details/${item.uuid}`)
 
-        <div>
-            <table className="table overflow-x-scroll">
-                <thead>
-                    <tr>
-                        <th scope="col">Equipo</th>
-                        <th scope="col">Recepcion</th>
-                        <th scope="col">Entrega</th>
-                        <th scope="col">Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        repairData?.map((item: ReparacionDto, index: number) => {
-                            return (
-                                <tr key={index}>
-                                    <td>{item.modelo}</td>
-                                    <td>{FormatMedDate(item.recepcion)}</td>
-                                    <td>{FormatMedDate(item.entrega)}</td>
-                                    <td>
-                                        <i onClick={
-                                            () => router.push(`/dashboard/reparaciones/details/${item.uuid}`)
+                                                    } className='m-2 bi bi-eye'></i>
+                                                </TableCell>
+                                            </TableRow>)
+                                    })
+                                }
+                            </TableBody>
+                        </Table>
 
-                                        } className='m-2 bi bi-eye'></i>
-                                    </td>
-                                </tr>)
-                        })
-                    }
-                </tbody>
+                    </TableContainer>
+                </Paper>
+            </Grid>
+            <Grid item sx={{position: "sticky",  bottom: 16, left: 16}}>
+                <Fab color="primary" aria-label="add">
+                    <Link href='/dashboard/reparaciones/first'>
+                        <Add sx={{ color: "white" }} />
+                    </Link>
+                </Fab>
+            </Grid>
 
-            </table>
+        </Grid>
 
-        </div>
 
-        <Fab sx={{ position: "sticky", bottom: "1rem", left: "1rem" }} color="primary" aria-label="add">
-            <Add />
-        </Fab>
+
     </>)
 }
 
