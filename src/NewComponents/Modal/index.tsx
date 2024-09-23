61ba17e0ae20049e011bfb3a85608d48632c80d0
Aviel Aldama <aviel.aldama@gmail.com>
2024-09-22 19:19:14 -0600

    pt-36

--- src/NewComponents/Modal/index.tsx
+++ src/NewComponents/Modal/index.tsx
@@ -1,7 +1,7 @@
 const Index = (props: { show: boolean, close: Function, children: string | JSX.Element | null }) => {
     return (<>
         <div className={`hs-overlay overflow-y-auto fixed inset-0 flex justify-center items-center transition-colors
-        ${props.show ? "visible bg-black/20" : "invisible"} pt-20`} >
+        ${props.show ? "visible bg-black/20" : "invisible"} pt-36 sm:pt-20`} >
             <div className="bg-white rounded-xl p-5 transition-all w-4/5 md:w-2/5 h-auto">
                 {props.children}
             </div>
