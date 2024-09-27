7425a69fc60e73887ff6ef614d90b5cc6bbd06ed
Aviel Aldama <aviel.aldama@gmail.com>
2024-09-27 15:19:34 -0600

    update component toast

--- src/NewComponents/Toast/index.tsx
+++ src/NewComponents/Toast/index.tsx
@@ -1,9 +1,11 @@
 
 const Index = (props: {Show: boolean, ServerMessage: {Succedded: boolean, Message: string}}) => {
-    let color = props.ServerMessage.Succedded ? "green" : "red"
     //${props.Show ? "visible": "invisible"}
     return (
-    <div className={`${props.Show ? "visible": "invisible"} z-40 absolute right-4 text-xs sm:text-base sm:right-24 md:right-56 lg:right-[350px] bg-${color}-100 text-${color}-700 rounded-lg border-${color}-200 px-4 py-3`}>
+    <div className={`${props.Show ? "visible": "invisible"} 
+    z-40 absolute right-4 text-xs sm:text-base sm:right-24 md:right-56 lg:right-[350px] 
+    bg-${props.ServerMessage.Succedded ? "green" : "red"}-100 text-${props.ServerMessage.Succedded ? "green" : "red"}-700 rounded-lg border-${props.ServerMessage.Succedded ? "green" : "red"}-200 
+    px-4 py-3`}>
         <div>
             <div className=''>
                 <div className=''>
