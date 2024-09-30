2f1fc86d056da142e4734f5cfe228469dc68188e
Aviel Aldama <aviel.aldama@gmail.com>
2024-09-30 09:07:00 -0600

    update promo ticket

--- src/Components/PromocionalLocalFix/page.tsx
+++ src/Components/PromocionalLocalFix/page.tsx
@@ -4,21 +4,23 @@ import React from 'react'
 
 const Page = React.forwardRef<HTMLDivElement>((props, ref) => {
   return (
-    <div ref={ref} className="">
-      <div className="">
+    <div ref={ref} className="w-[340pt] h-[380pt] p-8">
+      <div className="flex flex-col justify-center items-center">
         <div>
-          <h1 className="">LocalFix</h1>
-          <p>Reparacion de celulares y computadoras</p>
+          <h1 className="font-inspiration text-5xl font-bold text-center">LocalFix</h1>
+          <div className=''>
+          <p>Reparación de celulares y computadoras</p>
           <p>14 sur 6715 local A. Puebla Puebla</p>
+          </div>
         </div>
 
-        <div>
+        <div className=''>
           <h5><i className="bi bi-browser-safari"></i> www.localfix.mx</h5>
           <h4><i className="bi bi-whatsapp"></i> 2229713533</h4>
         </div>
 
-        <div className="">
-            <Image src={imgWhatsapp} alt='sin imagen' fill></Image>
+        <div className="w-[200pt] h-[280pt]">
+            <Image src={imgWhatsapp} alt='sin imagen'></Image>
         </div>
       </div>
     </div>
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,22 +2,21 @@
 import Promocional from '@/Components/PromocionalLocalFix/page'
 import { useRef, useState } from 'react';
 import { useReactToPrint } from 'react-to-print';
-import MenuAdd from "@/Components/AddMenu"
-import ModalGeneral from '@/Components/ModalGeneral/page'
-import FormDiagnostic from '@/Components/Formularios/AddDiagnosticForm/page'
 
 const Dashboard = () => {
     const componentRef = useRef<HTMLDivElement>(null);
-    const [modalDiag, setModalDiag] = useState(false)
-    const [modalRep, setModalRep] = useState(false)
 
     const handlePrint = useReactToPrint({
         content: () => componentRef.current,
     })
 
     return (<>
-    
-      
+        <div className='hidden'>
+            <Promocional ref={componentRef}></Promocional>
+        </div>
+        <div className='flex justify-center w-full'>
+            <button onClick={handlePrint} className='inline-flex justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'>Imprimir Promocional <i className="ml-2 bi bi-printer"></i></button>
+        </div>
     </>)
 }
 
