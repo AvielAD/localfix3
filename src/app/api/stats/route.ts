09e667190e545f1db257c84a8b051398336a3364
Aviel Aldama <aviel.aldama@gmail.com>
2024-12-04 15:17:18 -0600

    stats counter

---
+++ src/app/api/stats/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { StatsGeneral } from "@/DTOS/stats/stats.dto";
+
+export async function GET() {
+    let EventosView = {} as StatsGeneral
+    const testcookies = cookies().get('token')
+    try {
+        if (testcookies)
+            await fetch('https://localfixback2.localfix.mx/api/StatsGeneral', {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${testcookies.value}`
+                }
+            })
+                .then((response) => response.json())
+                .then((userInfo) => {
+                    EventosView = userInfo
+                }).catch((error) => {
+                })
+        if(EventosView)
+            return NextResponse.json(EventosView)
+        else
+            return NextResponse.json({})
+                
+    } catch (error) {
+        return NextResponse.json(EventosView)
+    }
+}
+
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,45 @@
 'use client'
+import { fetcher } from "@/Utilities/FetchHelper/Fetch.helper"
 import { BarBanner, CardBaner } from "@avielad/componentspublish"
+import useSWR from "swr"
 
 const Dashboard = () => {
+    const infoRepairs = useSWR('/api/stats', fetcher)
+    
+
+    const CardsBannerInfo = [
+        {
+            title: "En proceso",
+            value:  infoRepairs.data?.starts ?? "0",
+            icon: "bi bi-bar-chart-fill"
+        },
+        {
+            title: "Pausadas",
+            value: infoRepairs.data?.paused ?? "0",
+            icon: "bi bi-pie-chart-fill"
+        },
+        {
+            title: "Terminadas",
+            value: infoRepairs.data?.doneMounthCurrent ?? "0",
+            icon: "bi bi-bar-chart-steps"
+        },
+        {
+            title: "Entregadas",
+            value: infoRepairs.data?.deliveredMounthCurrent ?? "0",
+            icon: "bi bi-graph-up-arrow"
+        },
+        {
+            title: "Reparaciones",
+            value: infoRepairs.data?.repairsMounthCurrent ?? "0",
+            icon: "bi bi-bar-chart-steps"
+        },
+        {
+            title: "Diagnosticos",
+            value: infoRepairs.data?.repairsMounthCurrent ?? "0",
+            icon: "bi bi-graph-up-arrow"
+        }
+    ]
+
     return (<>
         <div className=''>
         <BarBanner title='Estadisticas' starmessage='Métricas localfix' arrowmessage='consideraciones más importantes'></BarBanner>
@@ -18,25 +56,3 @@ const Dashboard = () => {
 
 export default Dashboard
 
-const CardsBannerInfo = [
-    {
-        title: "En proceso",
-        value: "200",
-        icon: "bi bi-bar-chart-fill"
-    },
-    {
-        title: "Por entregar",
-        value: "200",
-        icon: "bi bi-pie-chart-fill"
-    },
-    {
-        title: "Mes actual",
-        value: "200",
-        icon: "bi bi-bar-chart-steps"
-    },
-    {
-        title: "Diagnosticos",
-        value: "200",
-        icon: "bi bi-graph-up-arrow"
-    }
-]
